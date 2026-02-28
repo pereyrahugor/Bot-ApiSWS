30558f76121ca36ba1c71ec797266cda6f35b478
pereyrahugor <pereyrahugor@gmail.com>
2026-02-28 17:59:09 -0300

    Update MovimientosApi.ts

--- src/API_SWS/MovimientosApi.ts
+++ src/API_SWS/MovimientosApi.ts
@@ -9,14 +9,14 @@ export class MovimientosApi {
    * Endpoint: POST /api/Movimientos/ObtenerSaldosDeCliente/
    * Request: { "cliente_id": ID }
    */
-  static async obtenerSaldosDeCliente(cliente_id: number) {
+  static async obtenerSaldosDeCliente(clienteId: number) {
     await ensureValidToken();
     const token = getSessionToken() || '';
     const url = `${BASE_URL}/api/Movimientos/ObtenerSaldosDeCliente/`;
     const headers = {
       'CURRENTTOKENVALUE': token
     };
-    const params = { clienteId: cliente_id };
+    const params = { clienteId };
     return axios.get(url, { headers, params });
   }
 }
